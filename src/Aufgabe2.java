c2e89dbb6ad8eb15f17363b36acdd14e84edcdd9
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-28 12:26:26 +0100

    Aufgabe3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -3,15 +3,21 @@ public class Aufgabe2
 {
     public static void main (String[]args)
     {
-        for (int i =0; i<3;i+=2)
+        for (int i =0; i<5;i++)
         {
             printName();
+            printAge(21+i);
         }
     }
 
     public static void printName()
     {
         System.out.println("Nina");
+    }
+
+    public static void printAge(int age)
+    {
+        System.out.println(age);
     }
 }
 
