b0a45d6fe27de8a50842a217e5a1ac5d9632c631
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-28 18:05:59 +0100

    Aufgabe5

---
+++ src/Rechnen.java
@@ -0,0 +1,19 @@
+public class Rechnen
+{
+    public static void main (String[]args)
+    {
+        System.out.println(add(5,5));
+        System.out.println(add(5.3, 5.4));
+    }
+
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static double add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
