97c69aa28f598b7c2d5383c5b28898ecd287dd36
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-28 11:57:46 +0100

    Aufgabe2

---
+++ src/Aufgabe2.java
@@ -0,0 +1,17 @@
+
+public class Aufgabe2
+{
+    public static void main (String[]args)
+    {
+        for (int i =0; i<3;i+=2)
+        {
+            printName();
+        }
+    }
+
+    public static void printName()
+    {
+        System.out.println("Nina");
+    }
+}
+
